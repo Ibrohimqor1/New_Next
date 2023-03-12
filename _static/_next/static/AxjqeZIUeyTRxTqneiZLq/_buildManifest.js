7256646553cd284c5d031d8be9dff704123d4428
Ibrohimjon  Muhammadjonov <115447690+Ibrohimqor1@users.noreply.github.com>
2023-03-12 20:26:53 +0300

    Add files via upload

---
+++ _static/_next/static/AxjqeZIUeyTRxTqneiZLq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/566-2e9a3420273896f4.js","static/chunks/pages/index-a5426e28e3c2d2b6.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
